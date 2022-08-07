553236d567286af57366ee55083b88c84c55c798
HARIV <kepa3715@gmail.com>
2022-08-07 11:54:14 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse('Привет, пока тут располагается пустая строка!')
+
+
+def groups_posts(request, slug):
+    return HttpResponse(f'Привет, ну и зачем ты запросил {slug} ?')
 # Create your views here.
